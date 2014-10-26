14255d6d33f10b31c91aa2100e0b71b662dee9dd
Flavio <flaviotosta7@gmail.com>
2014-10-26 16:18:31 -0200

    form ok

---
+++ pages/main_page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	 <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional: Include the jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<script src="../js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+
+</script>
+
+</head>
+<body>
+<header>
+	
+</header>
+</body>
+</html>
