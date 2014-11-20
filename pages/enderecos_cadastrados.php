212b7f954b92b22f3e18e0d5c4116b506b7e5274
Flavio <flaviotosta7@gmail.com>
2014-11-20 13:34:43 -0200

    dialogs de delete e update

--- pages/enderecos_cadastrados.php
+++ pages/enderecos_cadastrados.php
@@ -35,53 +35,113 @@
 					</ol>		
 				</div>
 
-				<div class="row content">
-					<div class="col-xs-12 col-md-6">
-						<div class="panel panel-default address">
-						<div class="panel-heading vcenter">
-							<span>Eduardo Frederico</span>		
-							<a href="" class="pull-right btn btn-default btn-sm">Excluir</a>
-							<a href="" class="pull-right btn btn-default btn-sm">Editar</a>
-						</div>
-					
-						<div class="panel-body">
-							<p>Av. José Vallim de Melo nº 116</p>
-							<p>Apartamento 302 Bloco b</p>
-							<p>Bairro Gameleira</p>
-						</div>
+				<div class="row content">				
+
+					<div class="col-xs-12">
+						<p class="text-left">
+							<a class="btn btn-default btn-sm" href="#">Adicionar endereço</a>
+						</p>
 					</div>						
-					</div>	
+
 					<div class="col-xs-12 col-md-6">
 						<div class="panel panel-default address">
-						<div class="panel-heading vcenter">
-							<span>Eduardo Frederico</span>		
-							<a href="" class="pull-right btn btn-default btn-sm">Excluir</a>
-							<a href="" class="pull-right btn btn-default btn-sm">Editar</a>
-						</div>
-					
-						<div class="panel-body">
-							<p>Av. José Vallim de Melo nº 116</p>
-							<p>Apartamento 302 Bloco b</p>
-							<p>Bairro Gameleira</p>
-						</div>
-						</div>						
-					</div>
-					<div class="col-xs-12 col-md-6">
-						<div class="panel panel-default address">
-						<div class="panel-heading vcenter">
-							<span>Eduardo Frederico</span>		
-							<a href="" class="pull-right btn btn-default btn-sm">Excluir</a>
-							<a href="" class="pull-right btn btn-default btn-sm">Editar</a>
-						</div>
-					
-						<div class="panel-body">
-							<p>Av. José Vallim de Melo nº 116</p>
-							<p>Apartamento 302 Bloco b</p>
-							<p>Bairro Gameleira</p>
-						</div>
+							<div class="panel-heading vcenter">
+								<span>Eduardo Frederico</span>		
+								<button type="button" class="pull-right btn btn-default btn-sm" 
+									data-toggle="modal" data-target="#modal-delete">
+									Excluir
+								</button>
+								
+								<button type="button" class="pull-right btn btn-default btn-sm" 
+									data-toggle="modal" data-target="#modal-edit" >
+									Editar
+								</button>
+							</div>
+
+							<div class="panel-body">
+								<p>Av. José Vallim de Melo nº 116</p>
+								<p>Apartamento 302 Bloco b</p>
+								<p>Bairro Gameleira</p>
+							</div>
 						</div>						
 					</div>					
 				</div>
+			</div>
+
+			<!-- Edit Modal Window -->
+			<div class="modal" id="modal-edit" tab-index="-1" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times</button>
+							<h4 class="modal-title">Editar Endereço</h4>
+						</div>
+						<div class="modal-body">
+							<form class="form-horizontal" role="form">
+								<div class="container-fluid">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="form-group">
+										<label class="control-label" for="name">Nome</label> 		
+										<input id="email" class="form-control" name="name" type="text" required>
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="address">Endereço</label> 		
+										<input id="address" class="form-control" name="address" type="text" required>
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="complement">Complemento</label> 		
+										<input id="complement" class="form-control" name="complement" type="text">
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="address">Bairro</label> 		
+										<input id="neighborhood" class="form-control" name="address" type="text" required>
+									</div>										
+									</div>									
+									<div class="col-xs-12 col-sm-5">
+										<div class="form-group">
+											<label class="control-label" for="phone1">Telefone 1</label> 		
+											<input id="phone1" class="form-control" name="phone1" type="text" required>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-5 col-sm-offset-1">
+										<div class="form-group">
+											<label class="control-label" for="phone2">Telefone 2</label> 		
+											<input id="phone2" class="form-control" name="phone2 " type="text">
+										</div>
+									</div>
+								</div>
+									
+								</div>
+							</form>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default">Salvar</button>
+							<button type="button" class="btn btn-default" 
+								data-dismiss="modal" data-target="#modal-edit">
+								Cancelar
+							</button>
+						</div>
+					</div>
+				</div>	
+			</div>
+
+			<!-- Delete Modal Window -->
+			<div class="modal" role="dialog" tab-index="-1" id="modal-delete">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times</button>
+							<h4 class="modal-title">Excluir Endereço</h4>
+						</div>
+						<div class="modal-content">
+							
+						</div>
+						<div class="modal-footer">
+							
+						</div>
+					</div>
+				</div>		
 			</div>
 		</div>
 	</div>
