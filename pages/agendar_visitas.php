97ab8a4bd2883a1e976394394795e92bf3331732
Flavio <flaviotosta7@gmail.com>
2014-11-23 06:40:49 -0200

    [carga horaria] ajustes

---
+++ pages/agendar_visitas.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../css/common.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+	<script src="../js/bootstrap.min.js"></script>
+
+	<script type="text/javascript"></script>
+
+	<style type="text/css">
+		
+
+	</style>
+
+</head>
+<body>
+	
+	<?php include("header.php"); ?>
+	
+	<div role="main">
+		<?php include("main_tabs.php"); ?>
+
+		<div id="load">		
+
+			<div class="container-fluid">
+				<div class="row">
+					<ol class="breadcrumb">
+						<li>Marcar Horários</li>
+						<li>Agendar Visitas</li>
+					</ol>		
+				</div>
+
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
